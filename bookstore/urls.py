ec781d0ca08651bbb761e4f7ac603bae9dd6bfad
NguyenTuKhai <ntukhai@gmail.com>
2017-03-21 21:13:26 +0800

    Adding simple registration with template base.html

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('store.urls')),
-    #url(r'^$', views.index, name='index'),
+    url(r'^', include('store.urls'), name='store'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
