14e960bbc4d6ffe240f4b4b5f5d7f136ae6f7729
NguyenTuKhai <ntukhai@gmail.com>
2017-04-02 01:20:52 +0800

    Add paypal button

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('store.urls'), name='store'),
+    url(r'^store/', include('store.urls'), name='store'),
+    #url(r'^', include('store.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
